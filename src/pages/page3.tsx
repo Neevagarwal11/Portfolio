d3e85f996c9503253a4a09d9a160e0a12cdfb410
Neev Agarwal <agarwalneev2005@gmail.com>
2024-09-14 00:07:35 +0530

    Project Page Partial completed

--- src/pages/page3.tsx
+++ src/pages/page3.tsx
@@ -17,8 +17,8 @@ function page3() {
                 <div className='w-full h-[100vh] border-2 flex items-center justify-center border-green-400'>
                 <div className={`internal h-[100%] w-[85%] absolute flex items-center border-2 ${index %2 ===0 ? "flex-row" :"flex-row-reverse"}`}>
 
-                <div className={`img-box w-[45%] h-[60%] rounded-[30px] border-2 absolute`}>
-                    <img src={item.img} alt="" />
+                <div className={`img-box w-[45%] h-[45%] rounded-[30px] overflow-hidden border-2 absolute`}>
+                    <img src={item.img} alt="" className='scale-[1.1]'/>
                 </div>
                 <div className={`text-8xl translate-y-[10vw]  absolute ${index %2 ===0 ? "left-[45%]" :"right-[45%]"}`}>{item.name}</div>
                 <div className={`text-2xl font-["para"]  border-2 w-[40%] absolute ${index %2 ===0 ? "right-2" :"left-2"}`}><p>{item.para}</p></div>
